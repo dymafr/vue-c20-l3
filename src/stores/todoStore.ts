1475751d008ea1fb3369b5e5f7c9480db1580ee3
dymafr <erwan1309@hotmail.com>
2022-04-08 15:18:54 +0200

    Initial commit

---
+++ src/stores/todoStore.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia';
+import { Todo } from '../interfaces/todo.interface';
+
+interface TodoState {
+  todos: Todo[];
+}
+
+export const useTodos = defineStore('todos', {
+  state: (): TodoState => ({
+    todos: [],
+  }),
+});
